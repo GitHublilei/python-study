6bb63976bc75131beca1e9c0bb4b913640f08816
lilei <572663541@qq.com>
2017-11-24 17:41:35 +0800

    11

--- test.py
+++ test.py
@@ -1,28 +1,30 @@
 from turtle import Turtle
-def tree(plist, l, a, f):       
-  if l > 5:        
-    lst = []         
-    for p in plist:             
+
+
+def tree(plist, l, a, f):
+  if l > 5:
+    lst = []
+    for p in plist:
       p.forward(l)
       q = p.clone()
-      p.left(a) 
+      p.left(a)
       q.right(a)
       lst.append(p)
-      lst.append(q)         
-    tree(lst, l*f, a, f)
-              
-def main():     
-  p = Turtle()     
-  p.color("green")     
-  p.pensize(5)     
-  p.hideturtle() 
-  p.speed(10)     
+      lst.append(q)
+    tree(lst, l * f, a, f)
+
+
+def main():
+  p = Turtle()
+  p.color("green")
+  p.pensize(5)
+  p.hideturtle()
+  p.speed(10)
   p.left(90)
-  p.penup() 
-  p.goto(0,-200)
+  p.penup()
+  p.goto(0, -200)
   p.pendown()
   t = tree([p], 200, 65, 0.637)
 
 
 main()
-      
